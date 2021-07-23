3e2708b47230ec67bc03cca7e59609d5265c832e
jakemraz <jakemraz100@gmail.com>
2021-07-23 09:48:01 +0000

    implement create token

--- functions/apis/create_token.py
+++ functions/apis/create_token.py
@@ -1,20 +1,48 @@
 import json
+import logging
+import os
+import time
+import boto3
+import secrets
+
+logger = logging.getLogger('create-token')
+logger.setLevel(logging.INFO)
+ddb = boto3.resource('dynamodb')
+table = ddb.Table(os.environ['OTT_TABLE'])
 
 def handler(event, context):
-    print('create_token')
-    print(event)
+    logger.info(event)
 
-    body = event.get('body', {})
-    body = json.loads(body) if body != {} else {}
-    print(body)
+    body = event.get('body', None)
+    body = json.loads(body) if body != None else None
+    if (body == None):
+        return respond(400, {
+            'code': 1,
+            'message': 'Missing Parameters'
+        })
+
+    id = body.get('id', None)
+    if (id == None):
+        return respond(400, {
+            'code': 1,
+            'message': 'Missing Parameters: id'
+        })
+    token = secrets.token_hex(nbytes=4)
+
+    table.put_item(
+        Item={
+            'token': token,
+            'id': id,
+            'TTL': int(time.time()) + 86400
+        }
+    )
 
     res = {
-        "code": 0,
-        "token": "1111"
+        'code': 0,
+        'token': token
     }
 
     return respond(200, json.dumps(res))
-
 
 def respond(statusCode, body):
     return {
